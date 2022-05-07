24ac178ecdc041e47d9fddc5db02594a6bd7b832
maabedelhay <mohamad.abedhay1@gmail.com>
2022-05-07 22:25:09 +0300

    Added post about bash

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-
+echo " "
 read -e -p 'Enter commit: ' -i "$message" message
 echo " " 
 echo "Sending changes to remote.." 
+echo " "
 git add .
 git commit -m "$message"
 git push -u origin main
 echo "  "
 echo "Building site... "
+echo " "
 hugo
 echo " "
 echo "Pushing public.. "
+echo " "
 cd public
 git add .
 git commit -m "$message"
